a967245bea2e9be2cb0360cc0d92ad3e3ddd0f4c
Sheerah <u2208900@live.warwick.ac.uk>
2025-02-07 12:52:48 +0000

    Test

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/api/test')
 def test():
-    return {'message': 'Connected to Flask!'}
+    return {'message': 'Connected to Flask! Lol'}
 
 if __name__ == '__main__':
     app.run(debug=True)
